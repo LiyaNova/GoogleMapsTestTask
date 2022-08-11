f77f9a43aec41695248254572bda2c68ef2ed7e0
Yuliya Nova <98648373+LiyaNova@users.noreply.github.com>
2022-08-11 18:38:36 +0300

    Update AppDelegate.swift

--- Maps/AppDelegate.swift
+++ Maps/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        GMSServices.provideAPIKey("AIzaSyBH5wuNzbVF3FMGEMcDqCRqW55l8dp6iVA")
+        GMSServices.provideAPIKey("YOUR_API_KEY")
 
         return true
     }
